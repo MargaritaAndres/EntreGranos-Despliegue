4bd5141af6b75e1bc309a559184d20a286eedf70
MargaritaAndres <135269185+MargaritaAndres@users.noreply.github.com>
2026-06-08 03:47:40 -0600

    Delete landing directory

--- landing/cerrar_sesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Inicia sesión para poder destruirla
-session_unset(); // Limpia todas las variables de sesión
-session_destroy(); // Destruye la sesión por completo
-
-// Redirige al login
-header("Location: Login.html");
-exit;
-?>
